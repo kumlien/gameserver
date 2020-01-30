d8f36fb552b70aed338ba665df93e9f03b456b25
Svante Kumlien <svante.kumlien@gmail.com>
2020-01-30 07:16:22 +0000

    wip

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 from http.server import HTTPServer
 from src.HttpServer import RequestHandler
 
 def main():
     server = HTTPServer(("0.0.0.0", 8080), RequestHandler)
     server.serve_forever()
-=======
-from src.HttpServer import Server
 
-def main():
-    server = Server()
-    server.m1()
->>>>>>> d6d7b74b1a48a54682aa71eb86cfcd462e51176d
 
 if __name__ == "__main__":
     main()
