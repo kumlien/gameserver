b56351cabecb346ea40c189022f92c6b4f37b329
Svante Kumlien <svante.kumlien@gmail.com>
2020-02-01 14:04:15 +0000

    wip bibblan

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
-from src.RequestHandler import RequestHandler
+from src.RequestHandler import MyRequestDispatcher
 
 PORT = 8080
 ADDRESS = ''
 
 #Entry point. Fire up a http server listening on ADDRESS:PORT
 #Let the RequestHandler handle the requests...
-def main(server_class=HTTPServer, handler_class=RequestHandler):
+def main(server_class=HTTPServer, handler_class=MyRequestDispatcher):
     server_address = (ADDRESS, PORT)
     httpd = server_class(server_address, handler_class)
+    print('Up and running on port', PORT)
     httpd.serve_forever()
 
 
