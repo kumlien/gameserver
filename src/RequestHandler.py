2fd26a873e86629e766cf4a8edcccb9ae536786c
Svante Kumlien <svante.kumlien@gmail.com>
2020-02-01 20:59:50 +0000

    wip

--- src/RequestHandler.py
+++ src/RequestHandler.py
@@ -19,28 +19,36 @@ class MyRequestDispatcher(BaseHTTPRequestHandler):
 
 
     def do_GET(self):
-        resource = self.extractResource(self.path)
+        (resource, id) = self.extractResourceAndId(self.path)
         print('GET called for ', resource)
         handler = MyRequestDispatcher.registry.getController(resource)
         print('Got a handler:', handler.__class__)
-        (code, headers, body) = handler.handleGet()
+        if id is None:
+            (code, headers, body) = handler.handleGet()
+        else:   
+            (code, headers, body) = handler.handleGet(id)
         print('Got ', code, headers, body, 'from controller')
         self.send_response(code, body)
         for header in headers.items():
             self.send_header(header[0], header[1])
+        self.end_headers()
         self.wfile.write(body.encode('utf8'))
+    
         
 
 
     # We only handle one level deep paths like 'users', not 'api/users'
-    def extractResource(self, path):
+    def extractResourceAndId(self, path):
         print('Extract resource part from path', path)
         resource = ''
+        id = None
         if path[0]=='/':
             path=path[1:]
 
         elements = re.split(r'/', path);
         if elements is not None and len(elements) > 0:
             resource = elements[0]
+        if len(elements) > 1:
+            id = elements[1]
         print('Extracted resource', resource)
-        return resource
+        return resource, id
--- src/controllers/UsersController.py
+++ src/controllers/UsersController.py
@@ -1,8 +1,36 @@
 import json
 
+'''
+Controller responsible or serving the 'users' resource. 
+For now we bundle the controller with the repository
+'''
+
+class UsersRepo(object):
+    users = {}
+
+class User():
+    def __init__(self, firstName, lastName, age):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+
+    
+
 class UsersController:
-    def handleGet(self):
-        print('Users controller called')
-        user = {'name': 'Svante', 'age': 47}
-        body = json.dumps(user)
-        return 200, {'Content-type': 'Application/json'}, body
+
+    def handleGet(self, id=None):
+        print('Users controller called with id', id)
+        body = ''
+        code = 200
+        if id is None:
+            print('Return all users')
+            body = json.dumps(UsersRepo.users.values)
+        elif id in UsersRepo.users:
+            user = UsersRepo.users[id]
+            user = User('Svante', 'Kumlien', '47')
+            body = json.dumps(user.__dict__)
+        else:
+            code = 404
+            body = 'This is not the user you are looking for...'
+
+        return code, {'Content-Type': 'Application/json; charset=UTF-8'}, body
